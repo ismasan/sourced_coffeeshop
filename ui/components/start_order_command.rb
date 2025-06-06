3981f2a25847a61116a7a3f898c0c8b26731905b
Ismael Celis <ismaelct@gmail.com>
2025-06-06 21:59:19 +0100

    Command button to start order

---
+++ ui/components/start_order_command.rb
@@ -0,0 +1,9 @@
+module Components
+  class StartOrderCommand < BaseComponent
+    def view_template
+      Sourced::UI::Components::Command(Order::Start, class: 'nice-form') do |form|
+        button(class: 'nice-button', type: 'submit') { 'Start order' }
+      end
+    end
+  end
+end
--- ui/pages/cashier_page.rb
+++ ui/pages/cashier_page.rb
@@ -1,15 +1,6 @@
 module Pages
   class CashierPage < Pages::Page
 
-    class CreateList < Phlex::HTML
-      def view_template
-        Sourced::UI::Components::Command(Todos::List::Create, class: 'nice-form') do |form|
-          form.text_field('name', required: true, autocomplete: 'off', class: 'nice-input')
-          button(class: 'nice-button', type: 'submit') { 'Create Todo List' }
-        end
-      end
-    end
-
     def initialize(layout: false)
       super(layout:)
     end
@@ -20,7 +11,9 @@ module Pages
 
     def container
       div id: 'main' do
-        h1 { 'Gello' }
+        div(class: 'actions') do
+          Components::StartOrderCommand()
+        end
       end
     end
   end
