db2f4888da69d78ee5ca1e9403c561a44ed24f1b
Ismael Celis <ismaelct@gmail.com>
2025-06-08 14:21:42 +0100

    Order: add item

--- domain/order.rb
+++ domain/order.rb
@@ -2,15 +2,41 @@ class Order < Sourced::Actor
   Start = Sourced::Command.define('orders.start')
   Started = Sourced::Event.define('orders.started')
 
-  Item = Data.define(:id, :name, :price, :quantity) do
-    def self.build(id:, name:, price: 0, quantity: 1)
-      new(id:, name:, price:, quantity:)
-    end
+  AddItem = Sourced::Command.define('orders.add_item') do
+    attribute :product_id, Types::String.present
+    attribute :variant_id, Types::String.present
+    attribute :product_name, Types::String.present
+    attribute :variant_name, Types::String.present
+    attribute :quantity, Types::Lax::Integer.default(1)
+    attribute :price, Types::Lax::Integer.default(0)
+  end
 
-    def total = price * quantity
+  ItemAdded = Sourced::Command.define('orders.item_added') do
+    attribute :product_id, String
+    attribute :variant_id, String
+    attribute :product_name, String
+    attribute :variant_name, String
+    attribute :quantity, Integer
+    attribute :price, Integer
   end
 
   class State
+    Item = Struct.new(:product_id, :variant_id, :product_name, :variant_name, :price, :quantity, keyword_init: true) do
+      def total = price * quantity
+      def id = [product_id, variant_id].join('-')
+
+      def self.build(product_id:, variant_id:, product_name:, variant_name:, quantity: 1, price: 0)
+        new(
+          product_id: product_id,
+          variant_id: variant_id,
+          product_name: product_name,
+          variant_name: variant_name,
+          quantity: quantity,
+          price: price
+        )
+      end
+    end
+
     attr_reader :id, :items
     attr_accessor :status
 
@@ -23,6 +49,11 @@ class Order < Sourced::Actor
     def total = items.values.sum(&:total)
 
     def open? = status == :open
+
+    def add_item(**kargs)
+      item = Item.build(**kargs)
+      @items[item.id] = item
+    end
   end
 
   state do |id|
@@ -37,5 +68,15 @@ class Order < Sourced::Actor
 
   event Started do |state, event|
     state.status = :open
+  end
+
+  command AddItem do |state, cmd|
+    raise ArgumentError, 'Order not open' unless state.open?
+
+    event ItemAdded, cmd.payload
+  end
+
+  event ItemAdded do |state, event|
+    state.add_item(**event.payload)
   end
 end
